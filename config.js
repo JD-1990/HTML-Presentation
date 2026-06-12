43c763953025fc909879b42d6db4bdb3eaaf9330
JD-1990 <166656615+JD-1990@users.noreply.github.com>
2026-06-12 21:49:39 +0200

    Create  config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Copy this file to config.js and fill in your real credentials.
+// config.js is gitignored and will never be committed to the repo.
+const ELEVENLABS_API_KEY  = "670c6115df32d84ddf877cde5a8bfd1da3f394bb945b2df83f882b3c5855b6dc";
+const ELEVENLABS_VOICE_ID = "UEKYgullGqaF0keqT8Bu";
+export { ELEVENLABS_API_KEY, ELEVENLABS_VOICE_ID };
