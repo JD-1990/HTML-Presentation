fd135c194c9030f65d27d534b6b577cdd408b6d2
JD-1990 <166656615+JD-1990@users.noreply.github.com>
2026-06-20 12:39:28 +0200

    Update ELEVENLABS_VOICE_ID in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Copy this file to config.js and fill in your real credentials.
 // config.js is gitignored and will never be committed to the repo.
 const ELEVENLABS_API_KEY  = "sk_239e4b4b50a9a0081bb71a55661d125d11bed95cdd890e7c";
-const ELEVENLABS_VOICE_ID = "B9PDs7mcHTMxHUw5U8Cf";
+const ELEVENLABS_VOICE_ID = "dVoi15NNDJligFBwnVO0";
 export { ELEVENLABS_API_KEY, ELEVENLABS_VOICE_ID };
